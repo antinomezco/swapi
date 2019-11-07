be49aafb115c144240dca14aa1ac8857fb9390e0
Constantino Saldana <constantino.saldana@gmail.com>
2019-11-07 20:55:34 +0000

    star wars

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import Contacts from './components/contacts';
 
 class App extends Component {
 
+    const urls = [
+      'https://swapi.co/api/people/1',
+      'https://swapi.co/api/people/2/?format=json',
+      'https://swapi.co/api/people/3/?format=json',
+      'https://swapi.co/api/people/4/?format=json'
+    ]
+
     state = {
         contacts: []
     };
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res => res.json())
+        Promise.all(urls.maps(url => {
+            fetch(url).then(res => res.json())
             .then((data) => {
                 this.setState({ contacts: data })
             })
-            .catch(console.log)
+        }
+        .catch(console.log)
     }
 
 
